bf72277272bc4d273a1f00b7ea0ade0f663126c0
Vijayram Patel <145386038+vijayrampatel@users.noreply.github.com>
2025-05-15 09:12:20 -0500

    debug statement added

--- tfidf_matcher.py
+++ tfidf_matcher.py
@@ -125,9 +125,8 @@ for record in json_data:
         print(f"Skipping {record['company']} - Email already sent")
         continue
 
-    if record["email_sent"] == True:
-        continue
-        
+    print("passed")
+    print(record)
     company_name = record["company"]
     job_url = record["url"]
     print(f"\nProcessing company: {company_name}")
